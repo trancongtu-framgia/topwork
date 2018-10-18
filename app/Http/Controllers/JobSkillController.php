24a93fac97db920d3030f8e5b3f563f5271f8772
trancongtu-framgia <tran.cong.tu@framgia.com>
2018-10-18 11:26:19 +0700

    [#2854][Create_Controller]

---
+++ app/Http/Controllers/JobSkillController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobSkill;
+use Illuminate\Http\Request;
+
+class JobSkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JobSkill  $jobSkill
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JobSkill $jobSkill)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JobSkill  $jobSkill
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JobSkill $jobSkill)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JobSkill  $jobSkill
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JobSkill $jobSkill)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JobSkill  $jobSkill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JobSkill $jobSkill)
+    {
+        //
+    }
+}
