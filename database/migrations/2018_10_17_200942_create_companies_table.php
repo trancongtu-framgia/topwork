a0b92287c9195de52d2c777d7a7c465cae2426bf
trancongtu-framgia <tran.cong.tu@framgia.com>
2018-10-18 12:58:25 +0700

    init migration

---
+++ database/migrations/2018_10_17_200942_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->string('address');
+            $table->string('range');
+            $table->string('country');
+            $table->string('working_day');
+            $table->text('description');
+            $table->string('logo_url');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
