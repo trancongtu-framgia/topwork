469dbfee5544ec9e220f69c0b5b9e43bbe868e12
tran.minh.hoang <FRAMGIA\tran.minh.hoang@framgia0303-lt.framgia.com>
2018-10-19 08:07:12 +0700

    add admin template

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,103 @@
+<header id="m_header" class="m-grid__item    m-header " m-minimize-offset="200" m-minimize-mobile-offset="200">
+    <div class="m-container m-container--fluid m-container--full-height">
+        <div class="m-stack m-stack--ver m-stack--desktop">
+            <!-- BEGIN: Brand -->
+            <div class="m-stack__item m-brand  m-brand--skin-light ">
+                <div class="m-stack m-stack--ver m-stack--general m-stack--fluid">
+                    <div class="m-stack__item m-stack__item--middle m-brand__logo">
+                        <a href="/" class="m-brand__logo-wrapper">
+                            <img alt="" src="{{ asset('assets/admin/images/logo.png') }}"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- END: Brand -->
+            <div class="m-stack__item m-stack__item--fluid m-header-head" id="m_header_nav">
+                <!-- BEGIN: Topbar -->
+                <div id="m_header_topbar" class="m-topbar  m-stack m-stack--ver m-stack--general">
+                    <div class="m-stack__item m-topbar__nav-wrapper">
+                        <ul class="m-topbar__nav m-nav m-nav--inline">
+                            <li class="m-nav__item m-topbar__user-profile  m-dropdown m-dropdown--medium
+                                m-dropdown--arrow  m-dropdown--align-right m-dropdown--mobile-full-width
+                                m-dropdown--skin-light" m-dropdown-toggle="click">
+                                <a href="#" class="m-nav__link m-dropdown__toggle">
+                                    <span class="m-topbar__userpic">
+                                        <img src="{{ asset('assets/admin/images/avatar.png') }}"
+                                                 class="m--img-rounded m--marginless m--img-centered" alt=""/>
+                                    </span>
+                                    <span class="m-nav__link-icon m-topbar__usericon  m--hide">
+                                        <span class="m-nav__link-icon-wrapper">
+                                            <i class="flaticon-user-ok"></i>
+                                        </span>
+                                    </span>
+                                    <span class="m-topbar__username m--hide">Nick</span>
+                                    <span class="m-topbar__username username-padding">
+                                        Mark Andre &nbsp;&nbsp;<i class="la la-angle-down"></i>
+                                    </span>
+                                </a>
+                                <div class="m-dropdown__wrapper">
+                                    <span class="m-dropdown__arrow m-dropdown__arrow--right
+                                        m-dropdown__arrow--adjust"></span>
+                                    <div class="m-dropdown__inner">
+                                        <div class="m-dropdown__header m--align-center">
+                                            <div class="m-card-user m-card-user--skin-light">
+                                                <div class="m-card-user__pic">
+                                                    <img src="{{ asset('assets/admin/images/avatar.png') }}"
+                                                         class="m--img-rounded m--marginless" alt=""/>
+                                                </div>
+                                                <div class="m-card-user__details">
+                                                    <span class="m-card-user__name m--font-weight-500">
+                                                            Mark Andre
+                                                    </span>
+                                                    <a href="" class="m-card-user__email
+                                                        m--font-weight-300 m-link">mark.andre@gmail.com
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="m-dropdown__body">
+                                            <div class="m-dropdown__content">
+                                                <ul class="m-nav m-nav--skin-light">
+                                                    <li class="m-nav__section m--hide">
+                                                        <span class="m-nav__section-text">Section</span>
+                                                    </li>
+                                                    <li class="m-nav__item">
+                                                        <a href="profile.html" class="m-nav__link">
+                                                            <span class="m-nav__link-title">
+                                                                <span class="m-nav__link-wrap">
+                                                                    <span class="m-nav__link-text">
+                                                                         {{ __('My Profile') }}
+                                                                    </span>
+                                                                </span>
+                                                            </span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="m-nav__item">
+                                                        <a href="profile.html" class="m-nav__link">
+                                                            <span class="m-nav__link-text">
+                                                                {{ __('Change password') }}
+                                                            </span>
+                                                        </a>
+                                                    <li class="m-nav__separator m-nav__separator--fit">
+                                                    </li>
+                                                    <li class="m-nav__item">
+                                                        <a href="snippets/pages/user/login-1.html"
+                                                           class="btn m-btn--pill    btn-secondary m-btn
+                                                               m-btn--custom m-btn--label-brand m-btn--bolder">
+                                                            Logout
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- END: Topbar -->
+            </div>
+        </div>
+    </div>
+</header>
